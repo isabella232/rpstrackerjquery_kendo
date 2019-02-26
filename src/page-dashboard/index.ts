c7c7bd9fcfd77f4168ee209b471768ba2e03884b
Alexander Ziskind <alex@nuvious.com>
2019-02-26 16:03:45 -0500

    switched to theme default-v2

--- src/page-dashboard/index.ts
+++ src/page-dashboard/index.ts
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
-import '@progress/kendo-ui/css/web/kendo.default.min.css';
+import '@progress/kendo-ui/css/web/kendo.default-v2.min.css';
 import '../styles.css';
 import './active-issues.css';
 
